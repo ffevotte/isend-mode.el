7e100775b5bcadf4608cd832cd89a6a4d414bcf1
François Févotte <fevotte@gmail.com>
2012-12-10 22:07:20 +0100

    Added tests for the various options
    It is a bit rustic, but better than nothing...

--- isend-mode.el
+++ isend-mode.el
@@ -20,8 +20,9 @@
 
 ;;; Commentary:
 
-;; If you make improvements to this code or have suggestions, please do not hesitate to fork the
-;; repository or submit bug reports on github. The repository is at:
+;; If you make improvements to this code or have suggestions, please do not
+;; hesitate to fork the repository or submit bug reports on github. The
+;; repository is at:
 ;;
 ;;     https://github.com/ffevotte/isend-mode.el
 
---
+++ test/test.el
@@ -0,0 +1,126 @@
+(defmacro isend--test (&rest body)
+  `(let ((destination (get-buffer-create "*isend test destination*"))
+         (origin      (get-buffer-create "*isend test origin*")))
+     (with-current-buffer destination
+       (erase-buffer))
+     (with-current-buffer origin
+       (setq buffer-read-only nil)
+       (erase-buffer)
+       (insert-file-contents "contents")
+       (setq buffer-read-only t)
+
+       (isend-associate destination)
+       (goto-char (point-min))
+       ,@body
+       (with-current-buffer destination
+         (buffer-substring (point-min) (point-max))))))
+
+(message "Isend -- running tests...")
+(setq isend-skip-empty-lines    nil
+      isend-strip-empty-lines   nil
+      isend-end-with-empty-line nil)
+
+
+
+(message "Sending line by line")
+(assert (string=
+ (isend--test
+  (deactivate-mark)
+  (dotimes (i 5)
+    (isend-send)))
+
+ "line1
+line2
+   
+line4 -- the line above contained only space
+line5
+"))
+
+
+
+(message "Sending a region")
+(assert (string=
+ (isend--test
+  (set-mark 0)
+  (activate-mark)
+  (goto-char 250)
+  (isend-send))
+
+ "line1
+line2
+   
+line4 -- the line above contained only space
+line5
+    line6     -- indented
+    line7     -- indented
+        line8 -- indented more
+    line9     -- indented
+line10
+"))
+
+
+
+(message "Sending line by line with `isend-skip-empty-lines'")
+(assert (string=
+ (isend--test
+  (let ((isend-skip-empty-lines t))
+    (deactivate-mark)
+    (dotimes (i 5)
+      (isend-send))))
+
+ "line1
+line2
+line4 -- the line above contained only space
+line5
+    line6     -- indented
+"))
+
+
+
+(message "Sending region with `isend-strip-empty-lines'")
+(assert (string=
+ (isend--test
+  (let ((isend-strip-empty-lines t))
+    (set-mark 0)
+    (activate-mark)
+    (goto-char 250)
+    (isend-send)))
+
+ "line1
+line2
+line4 -- the line above contained only space
+line5
+    line6     -- indented
+    line7     -- indented
+        line8 -- indented more
+    line9     -- indented
+line10
+"))
+
+
+
+(message "Sending region with `isend-end-with-empty-line'")
+(assert (string=
+ (isend--test
+  (let ((isend-end-with-empty-line t))
+    (set-mark 0)
+    (activate-mark)
+    (goto-char 250)
+    (isend-send)))
+
+ "line1
+line2
+   
+line4 -- the line above contained only space
+line5
+    line6     -- indented
+    line7     -- indented
+        line8 -- indented more
+    line9     -- indented
+line10
+
+"))
+
+
+
+(message "Isend -- all tests passed")
